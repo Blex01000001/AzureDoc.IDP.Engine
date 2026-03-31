98e8b5e23af1c31ee006b5353d41f0a7d95c6482
Alex <NB285>
2026-03-31 10:06:16 +0800

    add AzureSettings

---
+++ Models/AzureSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureDoc.IDP.Engine.Models
+{
+    public class AzureSettings
+    {
+        public string Endpoint { get; set; }
+        public string ApiKey { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace GoogleAIStudioProject
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine(">>>>START<<<<");
+
+
+            var service = new PdfTableConversionService();
+
+
+            var finalResult = await service.ExecuteAsync(@"C:\Users\01101006\Downloads\TEST\2.pdf");
+            Console.WriteLine($"page\tNPD\tTag No.\tItem\tL\tMinConfidence");
+            foreach (var item in finalResult)
+            {
+                Console.WriteLine($"{item.PageIndex}\t{item.Size}\t{item.TagNo}\t{item.Item}\t{item.L}\t{item.MinConfidence}");
+            }
+
+
+
+            //var fullText = await service.ReadAllTextAsync(@"H:\PDFs\32.pdf");
+            //Console.WriteLine("--- PDF 文字提取結果 ---");
+            //Console.WriteLine(fullText);
+
+
+            Console.ReadKey();
+        }
+    }
+}
